f2c3de5860431a549894b33e6007ae111087f595
thelastoutpostworkshop <thelastoutpostworkshop@gmail.com>
2026-06-29 08:04:00 -0400

    Extract ESP32-S3 USB-OTG profile

--- src/data/boards/esp32s3/index.ts
+++ src/data/boards/esp32s3/index.ts
@@ -1,2 +1,3 @@
+export { createEsp32s3UsbOtgProfile } from '@/data/boards/esp32s3/usbOtg';
 export { createEsp32s3UsbBridgeProfile } from '@/data/boards/esp32s3/usbBridge';
 export { esp32s3Mini1ModuleSource } from '@/data/boards/esp32s3/moduleSources';
